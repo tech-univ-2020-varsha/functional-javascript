1b20de17b008860c9fbf7a2ad2470021badcfd76
Varsha C L <clvarsha1998@gmail.com>
2020-01-14 01:56:10 +0530

    added test cases to check if returned value is an array and to check if all the values of the array is doubled

---
+++ basic-map/basicMap.test.js
@@ -0,0 +1,15 @@
+const doubleAll = require('./doubleAll');
+describe('the doubleAll function', () => {
+    it('should only return array', () => {
+        let numArray = [1, 2, 3];
+        const result = doubleAll(numArray);
+        expect(result).toBeInstanceOf(Array);
+    });
+
+    it('should double all the elements of the array', () => {
+        let numArray = [1, 2, 3];
+        const result = doubleAll(numArray);
+        expect(result.toString()).toEqual([2, 4, 6].toString());
+    });
+});
+
