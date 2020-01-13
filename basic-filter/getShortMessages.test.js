3731b365890dfc2060a109f561aa61677ab3b648
Varsha C L <clvarsha1998@gmail.com>
2020-01-14 03:05:31 +0530

    test cases to check 1) .message property 2)length of messages<50 3)return type of elements of the array

---
+++ basic-filter/getShortMessages.test.js
@@ -0,0 +1,44 @@
+const getShortMessages = require('./getShortMessages');
+
+describe('The getShortMessages funtion', () => {
+
+
+
+
+    it('should return array of messages that have <50 characters', () => {
+        const messageArray = [
+            { message: 'Esse id amet quis eu esse aute officia ipsum.' },
+            { message: 'Esse id amet quis eu esse aute officia ipsum.' }
+        ];
+
+        const result = getShortMessages(messageArray);
+        result.forEach(element => {
+            expect(element.length).toBeLessThan(50);
+        });
+    });
+
+    it('should return array of messages without their containing object', () => {
+        const messageArray = [
+            { message: 'Esse id amet quis eu esse aute officia ipsum.' },
+            { message: 'Esse id amet quis eu esse aute officia ipsum.' }
+        ];
+
+        const result = getShortMessages(messageArray);
+        result.forEach(element => {
+            expect(typeof element).toEqual("string")
+        });
+    });
+
+
+    it('should conatain array of objects with ".message" properties', () => {
+        const messageArray = [
+            { msg: 'Esse id amet quis eu esse aute officia ipsum.' },
+            { message: 'Esse id amet quis eu esse aute officia ipsum.' }
+        ];
+        expect(() => {
+            getShortMessages(messageArray);
+        }).toThrowError("all the objects of the array should have '.message' property");
+
+    });
+
+})
